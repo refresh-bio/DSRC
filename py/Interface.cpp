8fd10916a60891d9393316110340126f7a109b4a
lucas <lucas.roguski@gmail.com>
2013-12-03 01:41:37 +0100

    Updated header comment

--- py/Interface.cpp
+++ py/Interface.cpp
@@ -1,3 +1,12 @@
+/*
+  This file is a part of DSRC software distributed under GNU GPL 2 licence.
+  The homepage of the DSRC project is http://sun.aei.polsl.pl/dsrc
+  
+  Authors: Lucas Roguski and Sebastian Deorowicz
+  
+  Version: 2.00
+*/
+  
 #include <boost/python/detail/wrap_python.hpp>		// must be included before <Python>
 #include <boost/python/list.hpp>
 #include <boost/python/extract.hpp>
--- src/FastqIo.h
+++ src/FastqIo.h
@@ -2,7 +2,7 @@
   This file is a part of DSRC software distributed under GNU GPL 2 licence.
   The homepage of the DSRC project is http://sun.aei.polsl.pl/dsrc
   
-  Authors: Sebastian Deorowicz, Szymon Grabowski and Lucas Roguski
+  Authors: Lucas Roguski and Sebastian Deorowicz
   
   Version: 2.00
 */
